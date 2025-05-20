3a568dc9e418eacf920ca0eb7b7f38c7293de262
KasuvannagariSivaprasad <sivaprasad.522000@gmail.com>
2025-05-20 23:55:52 +0530

    Initial commit

---
+++ core/src/main/java/com/adobe/aem/guides/may/core/models/Demo.java
@@ -0,0 +1,9 @@
+package com.adobe.aem.guides.may.core.models;
+
+public interface Demo {
+
+    public String getArticleText();//we can give method name as per our requirement
+    public String getArticleDes();
+    public int getArticleNum();
+
+}
---
+++ core/src/main/java/com/adobe/aem/guides/may/core/models/DemoModel.java
@@ -0,0 +1,31 @@
+package com.adobe.aem.guides.may.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class DemoModel implements Demo{
+
+    @ValueMapValue
+    private String text;
+
+    @ValueMapValue
+    private String des;
+
+    @ValueMapValue
+    private int num;
+
+    public String getArticleText() {
+        return text;
+    }
+
+    public String getArticleDes() {
+        return des;
+    }
+
+    public int getArticleNum() {
+        return num;
+    }
+}
