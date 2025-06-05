e6d154acec6c315f06bf4a2c292ad21d99ebfbd6
KasuvannagariSivaprasad <sivaprasad.522000@gmail.com>
2025-06-05 15:20:07 +0530

    SlingAllMethods and PostMan

--- core/src/main/java/com/adobe/aem/guides/may/core/servlets/DemoServlet.java
+++ core/src/main/java/com/adobe/aem/guides/may/core/servlets/DemoServlet.java
@@ -1,35 +1,43 @@
 package com.adobe.aem.guides.may.core.servlets;
-
-import com.day.cq.commons.jcr.JcrConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletPaths;
 import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.propertytypes.ServiceDescription;
-
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
 @Component(service = { Servlet.class })
-@SlingServletPaths(
-        value="/bin/may/sample"
+@SlingServletResourceTypes(
+        resourceTypes  ="siva/prasad/developer"
 )
-@ServiceDescription("Simple Demo Servlet")
-public class DemoServlet extends SlingSafeMethodsServlet {
 
-    private static final long serialVersionUID = 1L;
+public class DemoServlet extends SlingAllMethodsServlet {
 
     @Override
-    protected void doGet(final SlingHttpServletRequest req,
-            final SlingHttpServletResponse resp) throws ServletException, IOException {
-        final Resource resource = req.getResource();
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Title = " + "Siva Prasad");
+    protected void doGet( SlingHttpServletRequest req, SlingHttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write(" Hello! Siva Prasad from Get()");
+    }
+
+    @Override
+    protected void doPost( SlingHttpServletRequest req, SlingHttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write(" Hello! Siva Prasad from Post()");
+    }
+
+    @Override
+    protected void doPut( SlingHttpServletRequest req, SlingHttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write(" Hello! Siva Prasad from Put()");
+    }
+
+    @Override
+    protected void doDelete( SlingHttpServletRequest req, SlingHttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write(" Hello! Siva Prasad from Delete()");
     }
 }
